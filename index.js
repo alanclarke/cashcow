843b0917f6b191cdec451d26cab9a802329e522e
alanclarke <hi@alz.so>
2016-10-26 14:07:53 +0100

    Apply slurry

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ module.exports = function cashcow (get, populate) {
   var farm = {}
 
   return function cowFetch (egg) {
+    let slurry = arguments
     if (farm[egg]) return farm[egg]
-    farm[egg] = get(egg).then(moo)
+    farm[egg] = get.apply(null, slurry).then(moo)
     return farm[egg].catch(cowpat)
 
     function moo (yolk) {
-      if (populate) if (yolk === void 0) return populate(egg).then(huzzah)
+      if (populate) if (yolk === void 0) return populate.apply(null, slurry).then(huzzah)
       return mop(yolk)
     }
 
